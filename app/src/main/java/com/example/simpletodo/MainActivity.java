4c3fecf41e4806f1e8b64e04ade63e0671e600ff
Terry Nguyen <terryhn1@uci.edu>
2020-01-07 00:38:47 -0800

    Adding the edit functionality

--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.simpletodo;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -22,6 +24,10 @@ import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
+
+        public static final String KEY_ITEM_TEXT = "item_text";
+        public static final String KEY_ITEM_POSITION = "item_position";
+        public static final int EDIT_TEXT_CODE = 20;
 
         List<String> items;
 
@@ -51,7 +57,20 @@ public class MainActivity extends AppCompatActivity {
               saveItems();
           }
         };
-        itemsAdapter = new ItemsAdapter(items, onLongClickListener);
+        ItemsAdapter.OnClickListener onClickListener = new ItemsAdapter.OnClickListener() {
+            @Override
+            public void onItemClicked(int position){
+                Toast.makeText(getApplicationContext(), "Item clicked at position" + position, Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(MainActivity.this, EditActivity.class);
+                //pass data being editted
+                i.putExtra(KEY_ITEM_TEXT, items.get(position));
+                i.putExtra(KEY_ITEM_POSITION, position);
+
+                //Display the activity
+                startActivityForResult(i, EDIT_TEXT_CODE);
+            }
+        };
+        itemsAdapter = new ItemsAdapter(items, onLongClickListener, onClickListener);
         rvItems.setAdapter(itemsAdapter);
         rvItems.setLayoutManager(new LinearLayoutManager(this));
 
@@ -71,6 +90,29 @@ public class MainActivity extends AppCompatActivity {
                 ;
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (resultCode == RESULT_OK && requestCode == EDIT_TEXT_CODE){
+            //Retrieve the updated text value
+            String itemText = data.getStringExtra(KEY_ITEM_TEXT);
+
+            // extract the original position of the edited item from the position key
+            int position = data.getExtras().getInt(KEY_ITEM_POSITION);
+
+            // update the model at the right position with new item text
+            items.set(position, itemText);
+
+            //notify the adapter
+            itemsAdapter.notifyItemChanged(position);
+            //persist the changes
+            saveItems();
+            Toast.makeText(getApplicationContext(), "Item updated successfully at position" + position, Toast.LENGTH_SHORT).show();
+
+        } else {
+            Log.w( "MainActivity", "Unknown call to onActivityResult");
+        }
     }
 
     private File getDataFile() {
